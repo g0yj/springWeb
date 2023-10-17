650b5430c7c16a96472614df178f0e94f86fad6f
새ë¡고연진 <jinyjgo@gmail.com>
2023-10-17 21:41:39 +0900

    231017 _ 과제2 하는중 ,,

--- src/main/java/example/day05과제2/PhoneService.java
+++ src/main/java/example/day05과제2/PhoneService.java
@@ -3,9 +3,11 @@ package example.day05과제2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PhoneService {
@@ -14,6 +16,8 @@ public class PhoneService {
 
     @PostMapping
     public boolean doPost(PhoneDto dto) {
+        System.out.println("PhoneService.doPost");
+        System.out.println("dto = " + dto);
         //1.DTO를 Entity로 변환
         PhoneEntity phoneEntity = PhoneEntity.builder()
                 .name(dto.getName())
@@ -21,20 +25,18 @@ public class PhoneService {
                 .build();
         //2.인터페이스명.save( 저장할 엔티티 )
         phoneEntityRepository.save(phoneEntity);
-
-
-        return false;
+        return true;
     }
 
     public List<PhoneDto> doGet() {
-        //1.모든엔티티호출 -> 인터페이스명.findAll()
-        // 반환타입 : 검색된 엔티티 여러개 List<Entity>
+        System.out.println("PhoneService.doGet");
+
         List<PhoneEntity> entities = phoneEntityRepository.findAll();
 
-        //2. List<Entity> 를 List<DTO> 로 변환
+        //List<Entity> 를 List<DTO> 로 변환
         List<PhoneDto> list = new ArrayList<>();
 
-        //3. 리스트 하나씩 꺼내기
+        // 리스트 하나씩 꺼내기
         entities.forEach(r -> {
             //3. 엔티티를 dto로 변환
             PhoneDto dto = PhoneDto.builder()
@@ -48,7 +50,22 @@ public class PhoneService {
         return list;
     }
 
+    public boolean doPut( PhoneDto dto) {
+        System.out.println("PhoneService.doPut");
+        System.out.println("put서비스실행");
+        Optional<PhoneEntity>entities=  phoneEntityRepository.findById(dto.getPno());
+        if(entities.isPresent()) {
+            PhoneEntity updateEntity = entities.get();
+            updateEntity.setName(dto.getName());
+            updateEntity.setNumber(dto.getNumber());
+        }
+        return true;
+    }
 
+    public boolean doDelete(int pno) {
+        phoneEntityRepository.deleteById(pno);
+        return true;
+    }
 
 
 
